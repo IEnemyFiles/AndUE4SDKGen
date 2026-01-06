b9cd009c12ffe404e5bd409fe46c7e20f755d8fb
IEnemyFiles <89214182+IEnemyFiles@users.noreply.github.com>
2026-01-06 13:40:56 +0700

    Add Dobby header file with SDKManager class

---
+++ jni/Dobby/dobby.h
@@ -0,0 +1,13 @@
+#ifndef DOBBY_H
+#define DOBBY_H
+
+#include <string>
+#include <vector>
+
+class SDKManager {
+public:
+    // Deklarasi fungsi statis
+    static void SaveSDK(std::string path, std::string gameShortName);
+};
+
+#endif
